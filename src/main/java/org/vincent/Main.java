2f8445ee7de8c36599067b406f9cfb60e8585f6c
Vincentius <vincentiussss@gmail.com>
2025-02-06 14:13:57 +0700

    fix(guesser): create guesser

--- src/main/java/org/vincent/Main.java
+++ src/main/java/org/vincent/Main.java
@@ -3,6 +3,7 @@ package org.vincent;
 public class Main {
     public static void main(String[] args) {
 
-        NumberScanner.inputReader();
+//        NumberScanner.inputReader();
+          NumberGuessingGame.numberGuesser();
     }
 }
---
+++ src/main/java/org/vincent/NumberGuessingGame.java
@@ -0,0 +1,28 @@
+package org.vincent;
+
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class NumberGuessingGame {
+    public static void numberGuesser(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Number Guesser game!");
+        System.out.println("Input your first number guess:");
+
+        int randomInt = ThreadLocalRandom.current().nextInt(1, 101);
+        int numberGuess = scanner.nextInt();
+        int guessCounter = 1;
+//        System.out.println(randomInt);
+        do {
+            if(numberGuess > randomInt){
+                System.out.println("Too high!");
+            } else if(numberGuess < randomInt) {
+                System.out.println("Too low!");
+            }
+            guessCounter++;
+            System.out.println("Input another number guess:");
+            numberGuess = scanner.nextInt();
+        } while (numberGuess!=randomInt);
+        System.out.println("Congrats you guess the correct number with "+guessCounter+" tries!");
+    }
+}
